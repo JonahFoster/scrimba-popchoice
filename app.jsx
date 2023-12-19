b74d3ea12acfe1724f0a5aaace91022a92b91e40
JonahFoster <81132130+JonahFoster@users.noreply.github.com>
2023-12-19 17:30:02 -0500

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { openai, supabase } from './config.js'
-import { createEmbeddings } from './processing.js'
 
 function App() {
     const [formStatus, setFormStatus] = useState("start")
